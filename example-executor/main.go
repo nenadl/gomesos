6c33c1128aeafd3aa6ab9508107a7c717bff0c71
Neno <nenadl@gmail.com>
2014-01-30 15:57:16 -0800

    initial commit

---
+++ example-executor/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"code.google.com/p/goprotobuf/proto"
+	"fmt"
+	"servers/gomesos"
+)
+
+func main() {
+	driver := gomesos.ExecutorDriver{
+		Executor: &gomesos.Executor{
+			Registered: func(
+				driver *gomesos.ExecutorDriver,
+				executor gomesos.ExecutorInfo,
+				framework gomesos.FrameworkInfo,
+				slave gomesos.SlaveInfo) {
+				fmt.Println("Executor registered!")
+			},
+
+			LaunchTask: func(driver *gomesos.ExecutorDriver, taskInfo gomesos.TaskInfo) {
+				fmt.Println("Launch task!")
+				driver.SendStatusUpdate(&gomesos.TaskStatus{
+					TaskId:  taskInfo.TaskId,
+					State:   gomesos.NewTaskState(gomesos.TaskState_TASK_RUNNING),
+					Message: proto.String("Go task is running!"),
+				})
+
+				driver.SendStatusUpdate(&gomesos.TaskStatus{
+					TaskId:  taskInfo.TaskId,
+					State:   gomesos.NewTaskState(gomesos.TaskState_TASK_FINISHED),
+					Message: proto.String("Go task is done!"),
+				})
+			},
+		},
+	}
+
+	driver.Init()
+	defer driver.Destroy()
+
+	driver.Run()
+}
---
+++ example-framework/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"code.google.com/p/goprotobuf/proto"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"servers/gomesos"
+	"strconv"
+)
+
+func main() {
+	taskLimit := 5
+	taskId := 0
+	exit := make(chan bool)
+	localExecutor, _ := executorPath()
+
+	master := flag.String("master", "localhost:5050", "Location of leading Mesos master")
+	executorUri := flag.String("executor-uri", localExecutor, "URI of executor executable")
+	flag.Parse()
+
+	executor := &gomesos.ExecutorInfo{
+		ExecutorId: &gomesos.ExecutorID{Value: proto.String("default")},
+		Command: &gomesos.CommandInfo{
+			Value: proto.String("./example-executor"),
+			Uris: []*gomesos.CommandInfo_URI{
+				&gomesos.CommandInfo_URI{Value: executorUri},
+			},
+		},
+		Name:   proto.String("Test Executor (Go)"),
+		Source: proto.String("go_test"),
+	}
+
+	driver := gomesos.SchedulerDriver{
+		Master: *master,
+		Framework: gomesos.FrameworkInfo{
+			Name: proto.String("GoFramework"),
+			User: proto.String(""),
+		},
+
+		Scheduler: &gomesos.Scheduler{
+			ResourceOffers: func(driver *gomesos.SchedulerDriver, offers []gomesos.Offer) {
+				for _, offer := range offers {
+					taskId++
+					fmt.Printf("Launching task: %d\n", taskId)
+
+					tasks := []gomesos.TaskInfo{
+						gomesos.TaskInfo{
+							Name: proto.String("go-task"),
+							TaskId: &gomesos.TaskID{
+								Value: proto.String("go-task-" + strconv.Itoa(taskId)),
+							},
+							SlaveId:  offer.SlaveId,
+							Executor: executor,
+							Resources: []*gomesos.Resource{
+								gomesos.ScalarResource("cpus", 1),
+								gomesos.ScalarResource("mem", 512),
+							},
+						},
+					}
+
+					driver.LaunchTasks(offer.Id, tasks)
+				}
+			},
+
+			StatusUpdate: func(driver *gomesos.SchedulerDriver, status gomesos.TaskStatus) {
+				fmt.Println("Received task status: " + *status.Message)
+
+				if *status.State == gomesos.TaskState_TASK_FINISHED {
+					taskLimit--
+					if taskLimit <= 0 {
+						exit <- true
+					}
+				}
+			},
+		},
+	}
+
+	driver.Init()
+	defer driver.Destroy()
+
+	driver.Start()
+	<-exit
+	driver.Stop(false)
+}
+
+func executorPath() (string, error) {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		return "", err
+	}
+
+	path := dir + "/example-executor"
+
+	return path, nil
+}
